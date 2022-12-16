643715af4898e92c60a1d8e648d95948c0b4ed2f
ilzoto <ilzoto@rarus.ru>
2022-12-16 16:54:40 +0400

    Fixed firebase init before app

--- src/main.js
+++ src/main.js
@@ -3,13 +3,22 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from '@/router/index.js';
 
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '@/firebase/firebase';
+
 const pinia = createPinia()
 
 pinia.use(({ store }) => {
     store.router = markRaw(router)
 })
 
-createApp(App)
-    .use(pinia)
-    .use(router)
-    .mount('#app')
+let app
+
+onAuthStateChanged(auth, () => {
+    if (!app) {
+        app = createApp(App)
+            .use(pinia)
+            .use(router)
+            .mount('#app')
+    }
+})
